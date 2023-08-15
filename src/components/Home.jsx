e1074853d42f4cf345cf5cd99b54422bb46eec21
muhammedmahmutoglu <muhammed.mahmutoglu@outlook.com>
2023-08-15 23:59:28 +0300

    complited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,59 +1,67 @@
-import React from "react";
-import sun from "../assets/image/Group.png";
-import sunTwo from "../assets/image/sun-2.png";
-function Home() {
-  return (
-    <div className="bg-primary col-12 col-sm-7 text-center pt-5 text-white">
-      <div>
-        <div className="container">
-          <div className="header">
-            <h1>San Frenciso</h1>
-            <span>clear</span>
-          </div>
-          <div className="mt-4">
-            <img src={sun} alt="sun" />
-          </div>
-          <div className="history">
-            <h3 className="my-2">11</h3>
-            <span className="h5">May xx 20xx</span>
-          </div>
-          <div></div>
-        </div>
-      </div>
+import React, { useEffect, useState } from "react";
 
-      <div className="d-flex justify-content-around border py-1">
-        <span>Monday</span>
-        <img src={sunTwo} alt="sun" />
-        <div>
-          <span>10</span>
-          <span className="mx-2 text-secondary">10</span>
+function Home() {
+  const key = "e74fe27c8816969e6b2b42b94360b611";
+
+  const [weatherInfo, setweatherInfo] = useState({})
+
+  const [citys, setCitys ] = useState("Adana");
+
+  const fecthData = async (selectedCitys) => {
+    const address = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCitys}&appid=${key}`;
+    const response = await fetch(address);
+    const data = await response.json();
+    setweatherInfo(data);
+  }
+
+  useEffect(() => {
+    fecthData(citys);
+  }, [citys]);
+
+  const handleCitysChange = (e) => {
+    setCitys(e.target.value);
+  };
+
+
+  // const getData = async function () {
+  //   const res = await fetch(adres);
+  //   const data = await res.json();
+  //   setweatherInfo(await data);
+  //   console.log(data);
+  // };
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+  return (
+    <div className="bg-primary col-12 col-sm-7 text-center pt-4 text-white">
+      <select
+        className="form-select form-select-lg mb-3"
+        aria-label="Large select example"
+        onChange={handleCitysChange} 
+        value={citys} 
+      >
+        <option value="Adana">Adana</option>
+        <option value="Sakarya">Sakarya</option>
+        <option value="Van">Van</option>
+        <option value="Eskisehir">Eskişehir</option>
+      </select>
+
+      <div className="container">
+        <div className="header">
+          {weatherInfo.name && <h1>{weatherInfo.name}</h1>}
+          {weatherInfo.weather && <span>{weatherInfo.weather[0].main}</span>}
+        </div>
+        <div className="mt-4">
+          {weatherInfo.weather && (
+            <img src={weatherInfo.weather[0].icon} alt="sun" />
+          )}
+        </div>
+        <div className="history">
+          {weatherInfo.sys && <h3 className="my-2">{weatherInfo.sys.country}</h3>}
+          {weatherInfo.coord && <span className="h5">Lon {weatherInfo.coord.lon}</span>}
         </div>
       </div>
-      <div className="d-flex justify-content-around border py-1" >
-        <span>Monday</span>
-        <img src={sunTwo} alt="sun" />
-        <div>
-          <span>10</span>
-          <span className="mx-2 text-secondary">10</span>
-        </div>
-      </div>
-      <div className="d-flex justify-content-around border py-1" >
-        <span>Monday</span>
-        <img src={sunTwo} alt="sun" />
-        <div>
-          <span>10</span>
-          <span className="mx-2 text-secondary">10</span>
-        </div>
-      </div>
-      <div className="d-flex justify-content-around border py-1" >
-        <span>Monday</span>
-        <img src={sunTwo} alt="sun" />
-        <div>
-          <span>10</span>
-          <span className="mx-2 text-secondary">10</span>
-        </div>
-      </div>    
-      
     </div>
   );
 }
